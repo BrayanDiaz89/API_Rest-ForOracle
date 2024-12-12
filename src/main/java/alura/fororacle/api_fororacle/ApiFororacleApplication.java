96bbf609516aee69399df2d9fe1a73685a81172a
Artu <brayandiaz258f@gmail.com>
2024-12-12 18:24:42 -0500

    First commit. Upload files

---
+++ src/main/java/alura/fororacle/api_fororacle/ApiFororacleApplication.java
@@ -0,0 +1,13 @@
+package alura.fororacle.api_fororacle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiFororacleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiFororacleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/alura/fororacle/api_fororacle/ApiFororacleApplicationTests.java
@@ -0,0 +1,13 @@
+package alura.fororacle.api_fororacle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiFororacleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
