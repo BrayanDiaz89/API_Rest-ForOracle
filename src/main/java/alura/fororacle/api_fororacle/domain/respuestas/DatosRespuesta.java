e80bd6c698499a902eda0d75cb8ba4c5fe84cf1e
Artu <brayandiaz258f@gmail.com>
2024-12-23 23:08:59 -0500

    Actualización datos respuesta, para retornar más especifico, los datos del autor.

--- src/main/java/alura/fororacle/api_fororacle/domain/respuestas/DatosRespuesta.java
+++ src/main/java/alura/fororacle/api_fororacle/domain/respuestas/DatosRespuesta.java
@@ -6,12 +6,16 @@ public record DatosRespuesta(Long id,
                              String contenido,
                              LocalDateTime fecha,
                              Long idEstudiante,
-                             Long idInstructor) {
+                             String nombreEstudiante,
+                             Long idInstructor,
+                             String nombreInstructor) {
     public DatosRespuesta(Respuesta respuesta) {
         this(respuesta.getId(),
                 respuesta.getContenido(),
                 respuesta.getFecha(),
                 respuesta.getEstudiante() != null ? respuesta.getEstudiante().getId() : null,
-                respuesta.getInstructor() != null ? respuesta.getInstructor().getId() : null);
+                respuesta.getEstudiante() != null ? respuesta.getEstudiante().getNombre() : "N/A",
+                respuesta.getInstructor() != null ? respuesta.getInstructor().getId() : null,
+                respuesta.getInstructor() != null ? respuesta.getInstructor().getNombre() : "N/A");
     }
 }
